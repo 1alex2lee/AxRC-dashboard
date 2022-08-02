20c946156bdc31a596e172f9c59048e1a0809654
Alex Lee <alexlee@dyn195142.shef.ac.uk>
2022-08-02 14:16:36 +0100

    initial commit

---
+++ src/webparts/amrcDashboard/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAmrcDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AmrcDashboardWebPartStrings' {
+  const strings: IAmrcDashboardWebPartStrings;
+  export = strings;
+}
